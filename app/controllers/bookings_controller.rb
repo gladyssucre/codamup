5f3480e203180a78a6587b30f55d97ed09522004
Gladys Sucre <gladyssucre@GLADYS.localdomain>
2017-10-13 10:50:37 +0200

    Through Association: Controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,17 @@
+class BookingsController < ApplicationController
+  class BookingsController < ApplicationController
+    def create
+      before_action :authenticate_user!
+      @booking = current_user.bookings.create(booking_params)
+
+      redirect_to @booking.event, notice: "Thank you for booking!"
+    end
+
+    private
+
+    def booking_params
+      params.require(:booking).permit(:starts_at, :ends_at, :price, :total, :event_id)
+    end
+  end
+
+end
